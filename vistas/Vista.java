1dac3d08223a97da5220b85f7e9495c0fee63076
Diana Carolina Hernández <dianacaro3552@gmail.com>
2016-05-23 17:25:33 -0500

    Agregando Contenido a los tabs

--- vistas/Vista.java
+++ vistas/Vista.java
@@ -14,6 +14,7 @@ public class Vista extends JFrame implements ActionListener{
   Container contenedor;
   JTabbedPane tabs;
   JPanel panelCirculares, panelEnlazadas, panelPilas;
+  JPanel grillaCirculares, grillaEnlazadas, grillaPilas;
   ArrayList<JButton> botonesCirculares, botonesEnlazadas, botonesPilas;
   //
   ListaC listaC;
@@ -58,21 +59,78 @@ public class Vista extends JFrame implements ActionListener{
     contenedor = getContentPane();
     contenedor.setLayout(new BorderLayout());
 
+    //Ejercicio Cirulares
+    grillaCirculares = new JPanel(new GridLayout(6,2));
+    grillaCirculares.add(new JLabel("1. "));
+    grillaCirculares.add(botonesCirculares.get(0));
+    grillaCirculares.add(new JLabel("2. "));
+    grillaCirculares.add(botonesCirculares.get(1));
+    grillaCirculares.add(new JLabel("3. "));
+    grillaCirculares.add(botonesCirculares.get(2));
+    grillaCirculares.add(new JLabel("4. "));
+    grillaCirculares.add(botonesCirculares.get(3));
+    grillaCirculares.add(new JLabel("5. "));
+    grillaCirculares.add(botonesCirculares.get(4));
+    grillaCirculares.add(new JLabel("6. "));
+    grillaCirculares.add(botonesCirculares.get(5));
+
     panelCirculares = new JPanel(new BorderLayout());
     panelCirculares.setBorder(BorderFactory.createTitledBorder("Ejercicio Listas Circulares"));
     panelCirculares.setVisible(true);
     panelCirculares.add(new JLabel("\tMenú 1. Listas Circulares"), BorderLayout.NORTH);
+    panelCirculares.add(grillaCirculares, BorderLayout.CENTER);
+
+    //Ejercicio Enlazadas
+    grillaEnlazadas = new JPanel(new GridLayout(10,2));
+    grillaEnlazadas.add(new JLabel("1. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(0));
+    grillaEnlazadas.add(new JLabel("2. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(1));
+    grillaEnlazadas.add(new JLabel("3. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(2));
+    grillaEnlazadas.add(new JLabel("4. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(3));
+    grillaEnlazadas.add(new JLabel("5. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(4));
+    grillaEnlazadas.add(new JLabel("6. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(5));
+    grillaEnlazadas.add(new JLabel("7. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(6));
+    grillaEnlazadas.add(new JLabel("8. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(7));
+    grillaEnlazadas.add(new JLabel("9. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(8));
+    grillaEnlazadas.add(new JLabel("10. "));
+    grillaEnlazadas.add(botonesEnlazadas.get(9));
 
     panelEnlazadas = new JPanel(new BorderLayout());
     panelEnlazadas.setBorder(BorderFactory.createTitledBorder("Ejercicio Listas Enlazadas"));
     panelEnlazadas.setVisible(true);
     panelEnlazadas.add(new JLabel("\tMenú 2. Listas Enlazadas"), BorderLayout.NORTH);
+    panelEnlazadas.add(grillaEnlazadas, BorderLayout.CENTER);
+
+    //Ejercicio Pilas
+    grillaPilas = new JPanel(new GridLayout(6,2));
+    grillaPilas.add(new JLabel("1. "));
+    grillaPilas.add(botonesPilas.get(0));
+    grillaPilas.add(new JLabel("2. "));
+    grillaPilas.add(botonesPilas.get(1));
+    grillaPilas.add(new JLabel("3. "));
+    grillaPilas.add(botonesPilas.get(2));
+    grillaPilas.add(new JLabel("4. "));
+    grillaPilas.add(botonesPilas.get(3));
+    grillaPilas.add(new JLabel("5. "));
+    grillaPilas.add(botonesPilas.get(4));
+    grillaPilas.add(new JLabel("6. "));
+    grillaPilas.add(botonesPilas.get(5));
 
     panelPilas = new JPanel(new BorderLayout());
     panelPilas.setBorder(BorderFactory.createTitledBorder("Ejercicio Pilas"));
     panelPilas.setVisible(true);
     panelPilas.add(new JLabel("\tMenú 3. Pilas"), BorderLayout.NORTH);
+    panelPilas.add(grillaPilas, BorderLayout.CENTER);
 
+    //General
     tabs.addTab("Listas Circulares", panelCirculares);
     tabs.addTab("Listas Enlazadas", panelEnlazadas);
     tabs.addTab("Pilas", panelPilas);
